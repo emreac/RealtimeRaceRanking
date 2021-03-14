5242647cf50800994d7af5766b5ab095670301d8
Emre Acikgoz <acikgozac@gmail.com>
2021-03-15 01:24:42 +0300

    RankingSystem

---
+++ Assets/script/GetDistances.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetDistances : MonoBehaviour
+{
+    Player1Move player1Move;
+    Player2Move player2Move;
+
+    void Start()
+    {
+        player1Move = GetComponent<Player1Move>();
+        player2Move = GetComponent<Player2Move>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {
+        Player1Dis();
+        Player2Dis();
+
+    }
+
+    void Player1Dis()
+    {
+        Debug.Log(player1Move.distanceTravelled);
+    }
+    void Player2Dis() {
+
+        Debug.Log(player2Move.distanceTravelled);
+
+    }
+}
